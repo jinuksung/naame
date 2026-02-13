edec428fa78ed914f67b9bc636f9fac1ec178dd1
jinuksoung <dev.sung3@gmail.com>
2026-02-13 22:42:06 +0900

    chore(web): add google site verification metadata

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -4,19 +4,20 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "네임핏 | 무료 작명 추천",
-  description: "외부 웹 전용 네임핏 무료 작명 추천"
+  description: "외부 웹 전용 네임핏 무료 작명 추천",
+  verification: {
+    google: "is91dFh-LIU76_RnaO5wWQvzcNZU1NYibUHwQ9URsJE",
+  },
 };
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: ReactNode;
 }>): JSX.Element {
   return (
     <html lang="ko">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
